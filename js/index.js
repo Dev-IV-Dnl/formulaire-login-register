4135817734a43f79c6269d39286dbdb3d8c1ce19
Dev-IV-Dnl <mattgiordanengo@gmail.com>
2021-10-08 12:22:41 +0200

    maj form

--- js/index.js
+++ js/index.js
@@ -11,6 +11,8 @@ btnRegister.addEventListener("click",()=>{
     console.log('Je clique sur le couton Register !');
     ovale.style.left = "50%";
     lr.style.marginLeft = "-100%";
+    btnLogin.style.color = "#333";
+    btnRegister.style.color = "#F1F1F1";
     // lr[i].style.transform = 'translate(0px, -'+(i*600)+'px)';
 })
 
@@ -18,5 +20,7 @@ btnLogin.addEventListener("click", () => {
     console.log('Je clique sur le couton Login !');
     ovale.style.left = "0px";
     lr.style.marginLeft = "0%";
+    btnRegister.style.color = "#333";
+    btnLogin.style.color = "#F1F1F1";
     // lr[i].style.transform = 'translate(0px, ' + (i * 600) + 'px)';
 })
