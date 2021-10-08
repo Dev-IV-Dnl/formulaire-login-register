e511f2d587534d83d69c5aaad2da841a20930bdf
Dev-IV-Dnl <mattgiordanengo@gmail.com>
2021-10-08 11:57:53 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+let lr = document.querySelector(".divLR");
+let btnLogin = document.querySelector(".btnLogIn");
+let btnRegister = document.querySelector(".btnRegister");
+let ovale = document.querySelector(".ovale");
+let divLogin = document.querySelector(".divLogIn");
+let divRegister = document.querySelector(".divRegister");
+let lCheck = document.querySelector(".Lcheck")
+
+
+btnRegister.addEventListener("click",()=>{
+    console.log('Je clique sur le couton Register !');
+    ovale.style.left = "50%";
+    lr.style.marginLeft = "-100%";
+    // lr[i].style.transform = 'translate(0px, -'+(i*600)+'px)';
+})
+
+btnLogin.addEventListener("click", () => {
+    console.log('Je clique sur le couton Login !');
+    ovale.style.left = "0px";
+    lr.style.marginLeft = "0%";
+    // lr[i].style.transform = 'translate(0px, ' + (i * 600) + 'px)';
+})
