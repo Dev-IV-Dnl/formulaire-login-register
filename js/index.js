ed6e0b6b67deacbaed213bcc323e15c68fdcfee6
Dev-IV-Dnl <mattgiordanengo@gmail.com>
2021-10-08 15:16:56 +0200

    maj BTN J/N OK !

--- js/index.js
+++ js/index.js
@@ -1,18 +1,24 @@
+// Variables pour le formulaire :
 let lr = document.querySelector(".divLR");
 let btnLogin = document.querySelector(".btnLogIn");
 let btnRegister = document.querySelector(".btnRegister");
 let ovale = document.querySelector(".ovale");
 let divLogin = document.querySelector(".divLogIn");
 let divRegister = document.querySelector(".divRegister");
-let lCheck = document.querySelector(".Lcheck")
+let lCheck = document.querySelector(".Lcheck");
+
+// Variables pour le Bouton Jour/nuit :
+let jn = document.querySelector(".btnJN");
+let cercle = document.querySelector(".cercle");
+let body = document.body;
 
 
 btnRegister.addEventListener("click",()=>{
     console.log('Je clique sur le couton Register !');
     ovale.style.left = "50%";
     lr.style.marginLeft = "-100%";
-    btnLogin.style.color = "#333";
-    btnRegister.style.color = "#F1F1F1";
+    btnLogin.style.color = "var(--col2)";
+    btnRegister.style.color = "var(--col3)";
     // lr[i].style.transform = 'translate(0px, -'+(i*600)+'px)';
 })
 
@@ -20,7 +26,19 @@ btnLogin.addEventListener("click", () => {
     console.log('Je clique sur le couton Login !');
     ovale.style.left = "0px";
     lr.style.marginLeft = "0%";
-    btnRegister.style.color = "#333";
-    btnLogin.style.color = "#F1F1F1";
+    btnRegister.style.color = "var(--col2)";
+    btnLogin.style.color = "var(--col3)";
     // lr[i].style.transform = 'translate(0px, ' + (i * 600) + 'px)';
+})
+
+jn.addEventListener("click", () => {
+    if(body.classList.contains("light")) {
+        body.classList.remove("light");
+        body.classList.add("dark");
+        cercle.style.left = "21px";
+    } else {
+        body.classList.remove("dark");
+        body.classList.add("light");
+        cercle.style.left = "2px";
+    }
 })
